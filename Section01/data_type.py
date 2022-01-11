3c40616a868655debbefe4a843d3732f18876326
name of user <email of user>
2022-01-11 21:51:35 +0900

    테이터 타입

---
+++ Section01/data_type.py
@@ -0,0 +1,29 @@
+'''
+    변수 이름을 짓는 규칙
+        1. 알파벳, _ , 숫자
+        2. 대소문자 구분
+        3. 변수명은 중복 X
+        4. 변수명 첫글자로 숫자를 사용X
+        5. 키워드와 동일하면 안됨
+        6. 한글 및 기타... 추천하지 않음
+
+    데이타 타입
+        숫자, 문자열, 논리값, 빈값
+'''
+text = 'Hello World'
+num = 10
+bool = True # False
+obj = None
+pi = 3.1415
+
+print(type(text)) # str --> 문자열 string
+print(type(num))  # int --> 정수 integer
+print(type(bool)) # bool --> 논리값 boolean
+print(type(obj))  # NoneType ---> 빈값
+print(type(pi))   # float ---> 실수
+
+
+
+
+
+
