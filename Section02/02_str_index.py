40684d162b82e062e2e3a79e0e271083644b14aa
name of user <email of user>
2022-01-13 19:47:28 +0900

    문자열 인덱스

---
+++ Section02/02_str_index.py
@@ -0,0 +1,25 @@
+'''
+    인덱스(index)
+        1. 각 문자마다 부여된 번호
+        2. 맨 앞에서부터 0으로 번호가 시작된다.
+        3. 맨 뒤에서부터는 -1로 번호가 시작된다.
+'''
+s = 'Hello'
+print(s[0],s[-5]) # H
+print(s[1],s[-4]) # e
+print(s[2],s[-3]) # l
+print(s[3],s[-2]) # l
+print(s[4],s[-1]) # o
+#s[2] = 'K' 부분 수정 X
+
+num = 100
+print(str(num)[0]) # 1
+print(str(num)[1]) # 0
+print(str(num)[2]) # 0
+
+
+
+
+
+
+
