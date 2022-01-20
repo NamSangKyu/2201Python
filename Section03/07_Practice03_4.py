b9b1ae00004b7827fa600af790d2bcff0e58e672
name of user <email of user>
2022-01-20 19:49:25 +0900

    문제 풀이

---
+++ Section03/07_Practice03_4.py
@@ -0,0 +1,6 @@
+travel = set()
+travel.add(input("희망하는 수학여행지를 입력 하세요 >>> "))
+travel.add(input("희망하는 수학여행지를 입력 하세요 >>> "))
+travel.add(input("희망하는 수학여행지를 입력 하세요 >>> "))
+
+print(f'조사된 수학여행지는 {travel}입니다.')
