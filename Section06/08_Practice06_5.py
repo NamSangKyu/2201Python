59428a355d5c39dcb488efff2fbff7129bbbdd1d
who <who@what.com>
2022-01-25 21:22:04 +0900

    while문

---
+++ Section06/08_Practice06_5.py
@@ -0,0 +1,28 @@
+n = 1
+while n <= 100:
+    if n % 10 == 0:
+        print(n)
+    else:
+        print(n, end="\t\t")
+    n+=1
+
+# n = 1
+# str = ''
+# while n <= 100:
+#     str += f'{n}\t\t'
+#     if n % 10 == 0:
+#         str += '\n'
+#     n += 1
+# print(str)
+
+# n = 1
+# i = 0
+# while i < 10:
+#     j = 0
+#     str = ''
+#     while j < 10:
+#         str += f'{n}\t\t'
+#         n+=1
+#         j+=1
+#     print(str)
+#     i+=1
