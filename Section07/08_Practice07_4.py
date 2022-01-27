6f516c8bcd370230c809928ee7076cccedefebd5
who <who@what.com>
2022-01-27 21:15:11 +0900

    for 문

--- Section07/08_Practice07_4.py
+++ Section07/08_Practice07_4.py
@@ -1,6 +1,8 @@
 exam = [99,78,100,91,81,85,54,100,71,50]
 
-for i in range(1, len(exam)):
+for i in range(len(exam)):
     if exam[i] + 5 <= 100:
         exam[i] += 5
+    else:
+        exam[i] = 100
 print(exam)
