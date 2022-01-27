d8572318a46898c1fbbf1f58290b2a3d4bd2d5e4
who <who@what.com>
2022-01-27 19:39:54 +0900

    for 문

---
+++ Section07/03_for_ex_2.py
@@ -0,0 +1,10 @@
+for i in range(5):#range(n) : 0 ~ n - 1 까지 숫자를 하나씩 뽑음
+    print(i,end=",")
+print("\n-------------------")
+for i in range(1,10):#range(s,e) : s ~ e - 1까지 숫자를 하나씩 뽑음
+    print(i, end=",")
+print("\n-------------------")
+#range(시작값,종료값,증감값)
+for i in range(1,10,2): #1부터 9까지 숫자를 하나씩 뽑는데 2씩 증가시켜가면서 뽑음
+    print(i, end=",")
+print("\n-------------------")
