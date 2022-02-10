bc8b3469fe5802c299b99d3c67b954364d5cf767
who <who@what.com>
2022-02-10 20:50:25 +0900

    set method

---
+++ Section10/04_set_method.py
@@ -0,0 +1,28 @@
+s1 = {'A','B','C','D'}
+s2 = {'C','D','G','H'}
+#교집합
+s3 = s1 & s2
+print(s3)
+s3 = s1.intersection(s2)
+print(s3)
+
+#합집합
+s3 = s1 | s2 #  | --> shift + \
+print(s3)
+s3 = s1.union(s2)
+print(s3)
+
+#차집합
+s3 = s1 - s2
+print(s3)
+s3 = s1.difference(s2)
+print(s3)
+
+
+
+
+
+
+
+
+
