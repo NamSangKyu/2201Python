103824586a666d6d6c090b9cc1c4a323a36893df
who <who@what.com>
2022-02-15 19:41:37 +0900

    함수

---
+++ Section11/01_function_1.py
@@ -0,0 +1,34 @@
+"""
+    함수 (Function)
+        자주 사용하는 기능을 내장함수 처럼 미리 만들어 놓은 코드
+        필요할 때마다 불러서 쓴다.
+
+    함수 문법
+    def 함수명([매개변수....]):
+        실행할 코드
+        [return 반환값]
+"""
+#인수 X, 반환값 X
+def printWelcome():
+    print('Hello Python')
+    print('Nice to meet you')
+
+printWelcome()
+
+#인수 X, 반환값 O
+def inputNumber():
+    no = int(input("숫자 하나 입력하세요 >>> "))
+    return no
+
+print(inputNumber() * 2)
+
+#인수 O, 반환값 X
+def printPersonInfo(name,age):
+    print(f'{name}님의 나이가 {age}살 입니다.')
+
+printPersonInfo("홍길동",20)
+
+
+
+
+
