a2208235d099c68fe39bfd7cda282b4c877a15ad
who <who@what.com>
2022-02-17 21:47:41 +0900

    로또

--- Section12/07_lotto.py
+++ Section12/07_lotto.py
@@ -11,4 +11,9 @@ while len(lotto_set) < 6:
     lotto_set.add(n)
 print('입력한 로또번호 목록 : {}'.format(lotto_set))
 
+#로또 당첨번호 랜덤하게 뽑음 1~45까지 숫자, 7개를 뽑음
+#마지막 번호는 보너스 번호로 취급
+winning_lotto_numbers = r.sample(range(1,46),7)
+print('당첨된 로또번호 목록 : {}'.format(winning_lotto_numbers))
 
+#당첨 등수 및 낙첨 정보 출력
