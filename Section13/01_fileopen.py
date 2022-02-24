65babeaf8c5e86db83c77b64c1445e5a604961cd
who <who@what.com>
2022-02-24 19:33:52 +0900

    파일 오픈

--- Section13/01_fileopen.py
+++ Section13/01_fileopen.py
@@ -9,8 +9,21 @@
 """
 #파일 열기
 #파일을 쓰기모드 열었음, 쓰기모드로 열면 파일을 매번 새로 생성
-file = open('firstFile.txt','wt',encoding="UTF-8")
+#파일을 생성할때는 폴더가 미리 만들어져 있어야 함, 폴더는 자동생성 X
+file = open('c:/test/firstFile.txt','wt',encoding="UTF-8")
 print('파일 오픈 완료')
 print(file)
 file.close()#파일 닫기
+
+with open('secondFile.txt','wt',encoding="UTF-8") as file:
+    print('파일 오픈 완료')
+    print(file)
+    #with 영역이 끝나면 자동으로 파일이 close 처리
+
+
+
+
+
+
+
 
