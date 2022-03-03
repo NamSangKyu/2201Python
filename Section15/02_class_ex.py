c1e94b31e0f31b01c7c59c3bd6b8bdd5dde4c251
who <who@what.com>
2022-03-03 19:48:03 +0900

    클래스 작성 예제

---
+++ Section15/02_class_ex.py
@@ -0,0 +1,15 @@
+#Person 클래스 --> 현재는 Person이라는 붕어빵 틀을 만듬
+class Person:
+    pass
+
+#메모리를 할당해서 하나씩 객체로 붕어빵처럼 찍어냄
+#생성을 할때 --> 클래스명()
+person1 = Person() #Person 객체하나 생성
+person2 = Person() #Person 객체하나 생성
+person3 = Person() #Person 객체하나 생성
+person4 = Person() #Person 객체하나 생성
+
+print(person1)
+print(person2)
+print(person3)
+print(person4)
