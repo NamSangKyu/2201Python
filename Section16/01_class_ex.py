2d40657750c91ea7126aec73ee275963188e6e59
who <who@what.com>
2022-03-04 19:44:07 +0900

    클래스 예제

---
+++ Section16/01_class_ex.py
@@ -0,0 +1,31 @@
+'''
+    TV
+        field
+            전원 - true/false
+            채널 - 정수 : 1 ~ 455
+            음량 - 정수 : 0 ~ 50
+        method
+            init --> 전원, 채널, 음량 필드를 선언하고 초기화하는 메서드
+            채널Up --> 실행 할때마다 채널 값을 증가, 마지막 채널값에서 증가하면 처음 채널로 이동
+'''
+class TV:
+    def init(self):
+        # __변수 --> private 멤버 -- 클래스 내부에서만 접근이 가능
+        self.__power = False
+        self.__ch = 24
+        self.__vol = 15
+
+    def ch_up(self):
+        self.__ch += 1
+        if self.__ch > 455:
+            self.__ch = 1
+        print(f'현재 채널 : {self.__ch}')
+
+tv = TV()
+tv.init()
+for i in range(500):
+    tv.ch_up()
+#print(tv.__ch)
+
+
+
