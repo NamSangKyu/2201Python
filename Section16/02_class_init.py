9ebc8474b5f97b735d4ae82794df0d8f509a1a6a
who <who@what.com>
2022-03-04 21:02:13 +0900

    생성자 예제

--- Section16/02_class_init.py
+++ Section16/02_class_init.py
@@ -16,3 +16,20 @@ p3 = Person()
 p1.print_info()
 p2.print_info()
 p3.print_info()
+
+class Animal:
+    def __init__(self, age):
+        self.age = age
+
+    def print_info(self):
+        print(f'이 동물의 나이 : {self.age}')
+
+animal = Animal(20)
+# animal = Animal() 생성자에 매개변수가 있으면 반드시 해당하는 데이터를 넣어야됨
+animal.print_info()
+
+
+
+
+
+
