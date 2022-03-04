8510ef051ac01bc7f610274f3430052eebb3ffea
who <who@what.com>
2022-03-04 20:30:17 +0900

    클래스 예제

--- Section16/01_class_ex.py
+++ Section16/01_class_ex.py
@@ -1,13 +1,16 @@
 '''
     TV
         field
-            전원 - true/false
+            전원 - True(On)/False(Off)
             채널 - 정수 : 1 ~ 455
             음량 - 정수 : 0 ~ 50
         method
             init --> 전원, 채널, 음량 필드를 선언하고 초기화하는 메서드
             채널Up --> 실행 할때마다 채널 값을 증가, 마지막 채널값에서 증가하면 처음 채널로 이동
             채널Down --> 실행 할때마다 채널 값을 감소, 처음 채널에서 감소하면 마지막 채널로 이동
+            음량Up --> 실행 할때마다 음량 값을 증가, 최대 음량에서는 더이상 증가가 안됨
+            음량Down --> 실행 할때마다 음량 값을 감소, 최소 음량에서는 더이상 감소가 안됨
+            전원OnOff --> 실행 할때마다 켜져 있으면 끄고, 꺼져 있으면 켜기
 '''
 class TV:
     def init(self):
@@ -29,13 +32,34 @@ class TV:
             self.__ch = 455
         print(f'현재 채널 : {self.__ch}')
 
+    def vol_up(self):
+        if self.__vol < 50:
+            self.__vol += 1
+        print(f'현재 음량 : {self.__vol}')
+
+    def vol_up(self):
+        if self.__vol > 0:
+            self.__vol -= 1
+        print(f'현재 음량 : {self.__vol}')
+
+    def power_on_off(self):
+        #True --> False, False --> True
+        self.__power = not self.__power
+        if self.__power:
+            print('TV 전원 On')
+        else:
+            print('TV 전원 Off')
 tv = TV()
 tv.init()
+tv.power_on_off()
+tv.power_on_off()
+tv.power_on_off()
+tv.power_on_off()
 # for i in range(500):
 #     tv.ch_up()
 #print(tv.__ch)
-for i in range(50):
-    tv.ch_down()
+# for i in range(50):
+#     tv.ch_down()
 
 
 
