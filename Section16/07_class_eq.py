3ba6cc79b174c95d505b6ed042f49ccc1e155ca0
who <who@what.com>
2022-03-08 19:33:14 +0900

    class eq

--- Section16/07_class_eq.py
+++ Section16/07_class_eq.py
@@ -15,7 +15,25 @@ class Number:
     # !=
     def __ne__(self, other):
         print('__ne__ 실행')
-        return  self.n1 != other.n1 or self.n2 != other.n2
+        # return  self.n1 != other.n1 or self.n2 != other.n2
+        return not self == other
+    # <
+    def __lt__(self, other):
+        print('__lt__ 실행')
+        return self.sum() < other.sum()
+    # <=
+    def __le__(self, other):
+        print('__le__ 실행')
+        return self.sum() <= other.sum()
+    # >
+    def __gt__(self, other):
+        print('__gt__ 실행')
+        return self.sum() > other.sum()
+
+    # >=
+    def __ge__(self, other):
+        print('__ge__ 실행')
+        return self.sum() >= other.sum()
 
 n1 = Number(10,20)
 n2 = Number(10,20)
@@ -24,3 +42,9 @@ print(n1,n2,n3,sep="\n")
 print(n1 == n2)
 print(n1 == n3) #인스턴스를 비교하면 일반적으로는 메모리 주소값으로 비교
 print(n1 != n2)
+n1.n1 = 5
+print(n1 != n2)
+print(n1 > n2)
+print(n1 >= n2)
+print(n1 < n2)
+print(n1 <= n2)
