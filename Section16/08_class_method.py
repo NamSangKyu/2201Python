c9acfb2bed6cc4dc246ad39eef168f7579476891
who <who@what.com>
2022-03-08 19:51:42 +0900

    class method

--- Section16/08_class_method.py
+++ Section16/08_class_method.py
@@ -6,6 +6,12 @@ class Count:
     def add_count(cls):
         cls.class_count += 1
 
+    @staticmethod
+    def static_method():
+        # 클래스 메서드나 변수에 직접 접근이 불가
+        print('인스턴스 생성 없이 사용가능한 메서드')
+        Count.class_count += 1 #클래스 명으로 접근했기때문에 접근이 가능
+
 c1 = Count()
 c2 = Count()
 
@@ -13,4 +19,6 @@ c1.add_count()
 c2.add_count()
 Count.add_count()
 print(c1.class_count)
+print(c2.class_count)
+Count.static_method()
 print(c2.class_count)
