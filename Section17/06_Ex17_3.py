7ed3711f3793864fd27746ee9dcc688bb528aca8
who <who@what.com>
2022-03-10 20:12:47 +0900

    Exception 처리

--- Section17/06_Ex17_3.py
+++ Section17/06_Ex17_3.py
@@ -1,6 +1,7 @@
 try:
     score = int(input('점수를 입력하세요 >>> '))
     if score < 0 or score > 100:
+        # Exception 강제 발생 - Exception 종류에 해당하는 객체를 생성
         raise Exception('점수는 0~100사이 입니다.')
     if score >= 80:
         print('{}점 합격입니다.'.format(score))
